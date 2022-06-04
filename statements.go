25bc1bff870be0ea9e10049e20dfe011b34201d5
kaanberke34 <berkeyilmaz34@gmail.com>
2022-06-05 00:29:13 +0300

    Statements

---
+++ statements.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	num1 := 18
+
+	if num1%2 == 0 {
+		fmt.Println("Given number", num1, " is even")
+	} else {
+		fmt.Println("Given number", num1, "is odd")
+	}
+
+}
